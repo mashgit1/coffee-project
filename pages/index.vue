4409f25562bfa0b5b8a5520bd583498ec4e7981b
Mariam Ashmawi <mariam18@uw.edu>
2024-05-09 20:20:16 -0700

    migrated project files, images, components

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,40 @@
-<template>
-  <Tutorial/>
-</template>
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../pages/HomeView.vue'
+import AboutView from '../pages/AboutView.vue'
+import CompareView from '../pages/CompareView.vue'
+import DrinksView from '../pages/DrinksView.vue'
+import 'bootstrap' 
+import 'bootstrap/dist/css/bootstrap.css' 
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-<script>
-export default {
-  name: 'IndexPage'
-}
-</script>
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'Images',
+    component: AboutView
+  },
+  {
+    path: '/compare',
+    name: 'Accordion',
+    component: CompareView
+  },
+  {
+    path: '/drinks',
+    name: 'Grid System',
+    component: DrinksView
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
